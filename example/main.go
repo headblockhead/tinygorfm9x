7209f6f5b04274f67370775d6342f14c0130d575
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-12-12 08:47:35 +0000

    refactor: linting updates - rename to Go standard names, lowercase errors, fix spelling of receive

--- example/main.go
+++ example/main.go
@@ -10,17 +10,17 @@ import (
 func main() {
 	time.Sleep(1 * time.Second)
 	device := tinygorfm9x.RFM9x{
-		SpiDevice: *machine.SPI1,
+		SPIDevice: *machine.SPI1,
 	}
 	print("Initializing device\r\n")
 	err := device.Init(tinygorfm9x.Options{
-		FrequencyMhz:      868,
+		FrequencyMHz:      868,
 		ResetPin:          machine.LORA_RESET,
 		CSPin:             machine.LORA_CS,
-		Dio0Pin:           machine.LORA_DIO0,
-		Dio1Pin:           machine.LORA_DIO1,
-		Dio2Pin:           machine.LORA_DIO2,
-		EnableCrcChecking: true,
+		DIO0Pin:           machine.LORA_DIO0,
+		DIO1Pin:           machine.LORA_DIO1,
+		DIO2Pin:           machine.LORA_DIO2,
+		EnableCRCChecking: true,
 	})
 	if err != nil {
 		print("Error initializing device: ", err, "\r\n")
@@ -42,7 +42,7 @@ func main() {
 		print("Error sending packet: ", err, "\r\n")
 	}
 	print("Starting recieve\r\n")
-	err = device.StartRecieve()
+	err = device.StartReceive()
 	if err != nil {
 		print("Error starting recieve: ", err, "\r\n")
 	}
